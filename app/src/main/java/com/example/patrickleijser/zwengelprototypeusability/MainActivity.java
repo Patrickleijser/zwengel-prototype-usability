5e4d52bf0625f743c1f48313e7ee638de71fd4a0
Patrick Leijser <patrickleijser@MacBook-Pro-van-Patrick.local>
2016-02-26 09:39:37 +0100

    Fragments implemented

--- app/src/main/java/com/example/patrickleijser/zwengelprototypeusability/MainActivity.java
+++ app/src/main/java/com/example/patrickleijser/zwengelprototypeusability/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.patrickleijser.zwengelprototypeusability;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,7 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, HighscoreFragment.OnFragmentInteractionListener, GoalsFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,20 +76,49 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_manage) {
-
+        Class fragmentClass;
+        switch(item.getItemId()) {
+            case R.id.goals:
+                fragmentClass = GoalsFragment.class;
+                break;
+            case R.id.highscore:
+                fragmentClass = HighscoreFragment.class;
+                break;
+            case R.id.shop:
+                fragmentClass = HighscoreFragment.class;
+                break;
+            case R.id.settings:
+                fragmentClass = HighscoreFragment.class;
+                break;
+            default:
+                fragmentClass = HighscoreFragment.class;
         }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        item.setChecked(true);
+        setTitle(item.getTitle());
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
